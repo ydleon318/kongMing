6ee9ecdd397e15ab9b7c8ba54e4a3d1c06349ada
yangdioffice <yangdi@hrbb.com.cn>
2019-11-12 20:38:33 +0800

    d

--- src/main/java/di/yang/controller/apiController/apiSingleTestController.java
+++ src/main/java/di/yang/controller/apiController/apiSingleTestController.java
@@ -5,8 +5,7 @@ import com.alibaba.fastjson.JSONObject;
 import di.yang.controller.BaseController;
 import di.yang.modle.api.ApiSingleTest;
 import di.yang.service.apiService.apiSingleTestService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import di.yang.utils.Tools;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,14 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/excute")
 public class apiSingleTestController extends BaseController {
-    private static Logger log = LoggerFactory.getLogger(apiSingleTestController.class.getName());
+//    private static Logger log = LoggerFactory.getLogger(apiSingleTestController.class.getName());
     @Autowired
     private apiSingleTestService apiservice;
 
     @PostMapping(value = "/addApiSingleCase")
     public ResponseEntity<?> addApiSingleCase(@RequestBody JSONObject param){
         ResponseEntity<?> result = null;
-        log.info("param is--->{}",param);
+        Tools.step("param is--->{}"+param);
         ApiSingleTest apiSingleTest = JSON.toJavaObject(param,ApiSingleTest.class);
         boolean flag = apiservice.addApiSingle(apiSingleTest);
         if (flag){
@@ -38,7 +37,7 @@ public class apiSingleTestController extends BaseController {
     @PostMapping(value = "/updataApiSingleTestCase")
     public ResponseEntity<?> updataApiSingleTestCase (@RequestBody JSONObject param){
         ResponseEntity<?> result = null;
-        log.info("param is--->{}",param);
+        Tools.step("param is--->{}"+param);
         ApiSingleTest apiSingleTest = JSON.toJavaObject(param,ApiSingleTest.class);
         boolean flag = apiservice.updataApiSingleTest(apiSingleTest);
         if (flag){
